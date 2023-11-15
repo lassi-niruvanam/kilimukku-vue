bbb0879607e88d84803b0e64c36d8bb283952c6b
julienmalard <julien.malard@mail.mcgill.ca>
2023-11-15 13:26:34 +0100

    பதிப்பு

--- மூலம்/கருவிகள்.ts
+++ மூலம்/கருவிகள்.ts
@@ -1,7 +1,7 @@
 import type { Ref } from "vue";
 
 export const குறிப்பின்_மதிப்பை_பெறு = <வ>(இ: வ | Ref<வ>): வ => {
-  return (இ as Ref<வ>).value ? (இ as Ref<வ>).value : (இ as வ);
+  return (இ as Ref<வ>)?.value ? (இ as Ref<வ>).value : (இ as வ);
 };
 
 export type குறிப்பு_அல்லது_மதிப்பு<வ> = வ | Ref<வ>;
