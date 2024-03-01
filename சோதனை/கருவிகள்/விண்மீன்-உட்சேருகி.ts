38f6ee1a702d2cdc1700400747530e254be056c3
julienmalard <julien.malard@mail.mcgill.ca>
2024-03-01 14:52:48 +0100

    {} இனிமேல் தேவையில்லை

--- சோதனை/கருவிகள்/விண்மீன்-உட்சேருகி.ts
+++ சோதனை/கருவிகள்/விண்மீன்-உட்சேருகி.ts
@@ -5,7 +5,7 @@ import { ClientConstellation, générerClient } from "@constl/ipa";
 export default ({ விண்மீன் }: { விண்மீன்?: ClientConstellation }) => {
   return {
     install: (செயலி: App) => {
-      const வாடிக்கையாளர் = விண்மீன் || générerClient({});
+      const வாடிக்கையாளர் = விண்மீன் || générerClient();
       செயலி.config.globalProperties.$constl = வாடிக்கையாளர்;
       செயலி.provide("constl", வாடிக்கையாளர்);
     },
